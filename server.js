8542a5b8048fcd2f2b1c75dc52efcdc647f9d14a
ThereWolf <bdunham484@gmail.com>
2022-07-15 15:20:35 -0500

    update port for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 //require data
 const { savedNotes } = require("./db/notes.json");
-
+const PORT = process.env.PORT || 3001;
 //instantiate the server
 const app = express();
 
@@ -28,6 +28,6 @@ app.get('/db/notes', (req, res) => {
 
 
 //tell server to listen to port 3001 via the listen() method
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`Note server now on port ${PORT}!`);
 });
