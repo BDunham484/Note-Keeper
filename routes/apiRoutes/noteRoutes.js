d72315d95400804c5ea0bba8d4eee2b91c8ff0cd
ThereWolf <bdunham484@gmail.com>
2022-07-17 18:43:39 -0500

    add front-end functionality

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,45 +1,64 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
-// const { validateNote, createNewNote } = require('../../server');
 const { savedNotes } = require("../../db/notes.json");
-
 
 
 router.get('/notes', (req, res) => {
     let results = savedNotes;
-    // console.log(req.query);
     res.json(results);
 });
 
 
-
-
-
 router.post('/notes', (req, res) => {
-    // console.log(req.body);
     if (!validateNote(req.body)) {
         res.status(400).send('The note is not properly formatted.');
     } else {
         const note = createNewNote(req.body, savedNotes);
-
+        
         res.json(note);
-    }
-
-    
+    };
 });
 
+
+router.delete(`/notes/:id`, (req, res) => {
+    let id = req.params.id;
+    deleteNote(id, savedNotes);
+    res.json(req.body);
+});
+
+
+
+
+//functions
 function createNewNote(body, savedNotesArr) {
     const note = body;
-
-    savedNotesArr.push(note)
+    const newNoteId = (note.id.replaceAll(" ","")) + (savedNotesArr.length.toString());
+    note.id = newNoteId;
+    
+    savedNotesArr.push(note);
 
     fs.writeFileSync(
         path.join(__dirname, '../../db/notes.json'),
         JSON.stringify({ savedNotes: savedNotesArr }, null, 2)
     );
-
     return note;
+};
+
+
+
+
+
+function deleteNote(id, savedNotesArr) {
+    let ids = savedNotes.map(element => {
+        return element.id
+    })
+    let index = ids.indexOf(id)
+    savedNotesArr.splice(index, 1)
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/notes.json'),
+        JSON.stringify({ savedNotes: savedNotesArr }, null, 2)
+    );
 }
 
 
@@ -50,9 +69,10 @@ function validateNote(note) {
     if (!note.title || typeof note.title !== 'string') {
         return false;
     }
-
     return true;
-}
+};
+
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,16 @@
-// const fs = require('fs');
-// const path = require('path');
-//import exress.js
 const express = require('express');
+
+
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
-//require data
-// const { savedNotes } = require("./db/notes.json");
 const PORT = process.env.PORT || 3001;
-//instantiate the server
 const app = express();
 
-
-
-
-//parse incoming string or array data
 app.use(express.urlencoded({ extended: true}));
-//parse incoming json data
 app.use(express.json());
 app.use(express.static('public'));
 
-
-
-
-
-
-
-
+//use routes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
