6e5814a524fde96fdc46cccbdbc5d1009f468c87
ThereWolf <bdunham484@gmail.com>
2022-07-17 21:03:07 -0500

    refactor

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const fs = require('fs');
-const path = require('path');
+const { createNewNote, deleteNote } = require('../../lib/notes')
 const { savedNotes } = require("../../db/notes.json");
 
 
@@ -11,13 +10,8 @@ router.get('/notes', (req, res) => {
 
 
 router.post('/notes', (req, res) => {
-    if (!validateNote(req.body)) {
-        res.status(400).send('The note is not properly formatted.');
-    } else {
-        const note = createNewNote(req.body, savedNotes);
-        
-        res.json(note);
-    };
+    const note = createNewNote(req.body, savedNotes);
+    res.json(note);
 });
 
 
@@ -26,52 +20,6 @@ router.delete(`/notes/:id`, (req, res) => {
     deleteNote(id, savedNotes);
     res.json(req.body);
 });
-
-
-
-
-//functions
-function createNewNote(body, savedNotesArr) {
-    const note = body;
-    const newNoteId = (note.id.replaceAll(" ","")) + (savedNotesArr.length.toString());
-    note.id = newNoteId;
-    
-    savedNotesArr.push(note);
-
-    fs.writeFileSync(
-        path.join(__dirname, '../../db/notes.json'),
-        JSON.stringify({ savedNotes: savedNotesArr }, null, 2)
-    );
-    return note;
-};
-
-
-
-
-
-function deleteNote(id, savedNotesArr) {
-    let ids = savedNotes.map(element => {
-        return element.id
-    })
-    let index = ids.indexOf(id)
-    savedNotesArr.splice(index, 1)
-    fs.writeFileSync(
-        path.join(__dirname, '../../db/notes.json'),
-        JSON.stringify({ savedNotes: savedNotesArr }, null, 2)
-    );
-}
-
-
-
-
-
-function validateNote(note) {
-    if (!note.title || typeof note.title !== 'string') {
-        return false;
-    }
-    return true;
-};
-
 
 
 
