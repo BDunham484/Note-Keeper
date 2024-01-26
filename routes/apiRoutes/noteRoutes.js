1e520d64f5cfa28c4c2c7dd553d5cf073d8c7d77
ThereWolf <bdunham484@gmail.com>
2024-01-25 21:29:05 -0600

    classwordk

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const { createNewNote, deleteNote } = require('../../lib/notes')
 const { savedNotes } = require("../../db/notes.json");
 
+const fs = require('fs');
+const path = require('path');
+
+
+
 
 router.get('/notes', (req, res) => {
     let results = savedNotes;
@@ -17,7 +22,17 @@ router.post('/notes', (req, res) => {
 
 router.delete(`/notes/:id`, (req, res) => {
     let id = req.params.id;
-    deleteNote(id, savedNotes);
+    let ids = savedNotes.map(element => {
+        return element.id
+    })
+    let index = ids.indexOf(id)
+    savedNotes.splice(index, 1)
+    // test = savedNotes.filter(x => x.id !== id);
+
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/notes.json'),
+        JSON.stringify({ savedNotes: savedNotes }, null, 2)
+    );
     res.json(req.body);
 });
 
